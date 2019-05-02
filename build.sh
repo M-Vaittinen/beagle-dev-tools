0aa4b7c70aa2743f5959d747395c6745a11130f8
Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>
2019-05-02 14:25:14 +0300

    add build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source ../use-kernel-dir
+
+cd ../bb-compiler/
+source setcc
+cd -
+
+if [ "$1" == "clean" ] || [ "$1" == "debug_cfg" ] || [ "$1" == "install" ] || [ "$1" == "dtb" ];
+then
+	make "$1"
+else
+	make
+fi;
