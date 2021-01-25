129965e4ec56a2cd6cfb9635b175be25a59f9b3e
Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>
2021-01-25 10:02:11 +0200

    read coefficients from file when computing tempertature (calib support)

--- smoker-project/meat_temp/meat_temp.c
+++ smoker-project/meat_temp/meat_temp.c
@@ -23,22 +23,56 @@
 #define S_C 0.0000003975024984
 */
 
-#define S_A 0.0008897101882
-#define S_B 0.0001760898686
-#define S_C 0.0000001472455495
+//#define S_A 0.0008897101882
+//#define S_B 0.0001760898686
+//#define S_C 0.0000001472455495
+
+#define COEFF_FILE "coeff_golden.txt"
+
+//first argument is input volts
+//Second argument is coefficient file
+
+int get_coeff_100(FILE *r, long double *S_A, long double *S_B, long double *S_C)
+{
+	int i;
+	long double S[3];
+
+	for (i = 0; i < 3; i++)
+		if (1 != fscanf(r, "%Lf\n", &S[i]))
+			return -1;
+	*S_A = S[0];
+	*S_B = S[1];
+	*S_C = S[2];
+
+	return 0;
+}
 
 int main(int argc, char * argv[])
 {
 	long double therm_resistance;
-	long double temp;
+	long double temp, S_A, S_B, S_C;
 	long double raw_volts;
 	double cterm;
 	double bterm;
 	double divider;
 	char *chkptr;
+	FILE *f;
 
-	if (argc != 2)
+	if (argc != 2 && argc != 3)
 		return -1;
+
+	if (argc == 3)
+		f = fopen(argv[2],"r");
+	else
+		f = fopen(COEFF_FILE, "r");
+
+	if (!f)
+		return -1;
+
+	if(get_coeff_100(f, &S_A, &S_B, &S_C))
+		return -1;
+
+	fclose(f);
 
 	raw_volts = (double)strtol(argv[1], &chkptr, 0);
 	if (0 == raw_volts)
@@ -65,7 +99,8 @@ int main(int argc, char * argv[])
 	divider = S_A + bterm + cterm;
 //	printf("divier = %f\n", divider);
 
-	temp = 1.0 / divider;
+//	temp = 1.0 / divider;
+	temp = 100.0 / divider;
 
 	printf("%f\n",(double)temp-273.15);
 	return 0;
