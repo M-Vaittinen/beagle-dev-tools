fbe3fdd79da3590044372ad9274822c2325c4bce
Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>
2021-01-26 09:11:29 +0200

    translate calibration page and fix some errors from description

--- smoker-project/meat_temp/web/html/calibrate.php
+++ smoker-project/meat_temp/web/html/calibrate.php
@@ -5,29 +5,26 @@
 <body>
 </body>
 
-<h1>Kalibroi lihalämpömittari</h1>
-<p>
-Kalbroi (uusi) lihalämpömittarin anturi seuraavasti:
+<h1>Meat thermometer probe calibration</h1>
 <p>
 <ul>
-<li>Ota joku muu lämpömittari joka kestää kuumaa. Laita se ja blackburnin mittari astiaan.</li>
-<li>Kaada asitiaan kuumaa vettä ja katso lämpötila toimivasta mittarista</li>
-<li>Syötä lämpötilaarvo sivulla olevaan kentään ja paina "mittaa"</li>
-<li>Syötä huomattavasti alhaisempi lämpö seuraavaan kenttään ja veden jäähdyttyä tähän lämpötilaan paina jälleen "mittaa"</li>
-<li>Viimeiseksi voit laittaa astiaan kylmää vettä ja syöttää veden lämpötilan viimeiseen kenttään ja painaa "mittaa"</li>
-<li>Kun olet mitannut kakki kolme arvoa paina "tallenna kalibrointi"</li>
+<li>Get working thermometer which can measure temperatures at calibration range</li>
+<li>Pour hot water in a bowl. Put both probes in water and check the water temperature from working thermometer</li>
+<li>Fill the measured temperature in form and select 'measure'</li>
+<li>Cool down the water to significantly lower temperature, fill in the new temperature and press 'measure' again</li>
+<li>Fill bowl with cold water, fill in last temperature and press 'measure'</li>
+<li>When you have measured all three temperature/resistance pairs the calibration is saved</li>
 </ul>
 <p>
-<h3>Ohjeita kalibrointiin käytettävien lämpöarvojen valintaan</h3>
+<h3>Guide to select calibration temperatures</h3>
 <ul>
-<li>Kahden arvon olisi hyvä olla mitattavan lämpötila-alueen ääripäistä. Esim 10 C - 80 C</li>
-<li>Keskimmäisen arvon olisi hyvä olla näiden puolivälistä, mielellään kuitenkin lähempänä kuuminta arvoa kuin viileintä</li>
+<li>Two of the values should be from the extremes of temperature range you think food will be. Good values could be 10 C and 80 C</li>
+<li>The mid value could be somewhere in between. Preferably closer the high than low-end of range. Mid value should work Ok.</li>
 </ul>
 <p>
-Savustin tallentaa yhden aiemman kalibroinnin palautusta varten.<br>
-Mikäli kalibrointi epäonnistuu voit palauttaa edellisen käytetyn kalibroinnin klikkaamalla tällä
-sivulla "palauta"<br>
-
+<!-- Smoker saves one previous calibration for restoring.<br>
+TODL: If the calbration fails you should be able to restore previuously used calibration by clicking 'restore'<br>
+-->
 <table border="1">
 
 <?php
@@ -51,28 +48,22 @@ echo '<tr> <td>';
 		if (isset($_POST[$res[$i]]) && ctype_digit($_POST[$res[$i]]))
 		{
 			$resistance = $_POST[$res[$i]];
-//			echo "resistance".$i." was set. Value ".$resistance;
 		}
 		else {
-//			echo "resistance".$i." was not set. Reading and writing to ".$res[$i];
-//			echo 'calling system("/savustin/bin/read-resistance.sh /'.$res[$i].'");'; 
 			system("/savustin/bin/read-resistance.sh /savustin/tmp/".$res[$i]);
-//			echo 'reading measured resistance: trim(file_get_contents("/savustin/tmp/'.$res[$i].'"));';
 			$resistance = trim(file_get_contents("/savustin/tmp/".$res[$i]));
-//			echo "Just Read resistance ".$i." value ".$resistance;
-//			echo ' resistance'.$resistance;
 		}
 		echo ' resistance '.$resistance;
 		echo '<input type="hidden" name="'.$res[$i].'" value="'.$resistance.'"/></td> </tr>';
 	} else {
 		echo '<input type="text" name="'.$temps[$i].'"/>';
-		echo '<input type="submit" name="foobar" value="mittaa"/></td></tr>';
+		echo '<input type="submit" name="foobar" value="measure"/></td></tr>';
 	}
 }
 echo '</form></table>';
 if ($ctr == 3) {
 	system("/savustin/bin/prepare_tempres.sh ".$_POST[$temps[0]]." ".$_POST[$temps[1]]." ".$_POST[$temps[2]]." /savustin/tmp/".$res[0]." /savustin/tmp/".$res[1]." /savustin/tmp/".$res[2]);
-	die('kalibroitu <a href="index.php">PÄÄSIVULLE</a></html>');
+	die('Calibration done <a href="index.php">MAIN PAGE</a></html>');
 }
 ?>
 
