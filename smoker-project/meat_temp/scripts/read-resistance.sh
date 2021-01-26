083b30b3ca324a5405760578065b59baab941b41
Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>
2021-01-26 08:38:09 +0200

    remove obsolete dublicates

--- smoker-project/meat_temp/scripts/read-resistance.sh
+++ smoker-project/meat_temp/scripts/read-resistance.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-#Read ADC value and call executable which calcs resistance.
-#First and only parameter for this should be output file where
-#resistance is written
-#give $1 as parameter for get_resistance
+# Use script to read ADC value (script does required filtering) and
+# call binary which converts the ADC value to resistance.
+#
+# First and only parameter for this should be output file where
+# resistance is written
+# give $1 as parameter for get_resistance
 #
 
-ADC_FILE=/read_meat_temp.sh
+ADC_FILE=/savustin/bin/read_meat_temp.sh
 RAW_VOLT=`$ADC_FILE`
 
-/home/debian/bin/get_resistance $RAW_VOLT $1
+/savustin/bin/get_resistance $RAW_VOLT $1
