02061c1e02fd2dd0dc4481a9670341da1f866fd2
Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>
2021-01-26 08:21:00 +0200

    document prepare_tempres.sh

--- smoker-project/target_fs/savustin/bin/prepare_tempres.sh
+++ smoker-project/target_fs/savustin/bin/prepare_tempres.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-
-
+#
+# Helper script called by php script which collects the food temperature probe
+# calibration values. User should put the food probe in three known
+# temperatures (like use working food probe and a pot with cold water,
+# warm water and hot water) and initiate food probe resistance measurements
+# in these known temperatures.
+#
+# PHP script collects the temperatures from user and initiates resistance
+# measurements - and passes the values to this script.
+#
+# This script formats calib_vals.txt file with format that the calc_coeff
+# program expects - and runs the program. calc_coeff then computes the
+# coefficient values for Steinhart-Hart equation (which we use to convert
+# measured NTC food-probe resistances to temperatures when we measure food
+# temperature).
 
 TEMP0=$1
 TEMP1=$2
